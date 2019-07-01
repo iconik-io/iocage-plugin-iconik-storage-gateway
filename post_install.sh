5b76ab4eceb6703091e514b020188ede1edf1f5d
Martin Wilke <miwi@ixsystems.com>
2019-07-01 22:49:54 +0800

    - Fix permission - Make it reboot safe - Added package installation

--- post_install.sh
+++ post_install.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-service iconik_storage_gateway start
+sysrc -f /etc/rc.conf iconik_storage_gateway_enable="YES"
+
+echo "Download Distfiles"
+cd /root
+fetch https://storage.googleapis.com/harald-iconik-test/iconik_storage_gateway-1.6.0.txz
+
+pkg add /root/iconik_storage_gateway-1.6.0.txz
+
+#restart services
+/usr/local/etc/rc.d/iconik_storage_gateway restart
+
