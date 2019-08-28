2e03f44ffd4b447e3a2272061b7a7270e34dc43d
Eugene <eugene@cantemo.com>
2019-08-28 14:12:12 +0200

    Update package url

--- post_install.sh
+++ post_install.sh
@@ -4,9 +4,9 @@ sysrc -f /etc/rc.conf iconik_storage_gateway_enable="YES"
 
 echo "Download Distfiles"
 cd /root
-fetch https://storage.googleapis.com/harald-iconik-test/iconik_storage_gateway-1.6.0.txz
+fetch https://downloads.iconik.io/iconik_storage_gateway-latest.txz
 
-pkg add /root/iconik_storage_gateway-1.6.0.txz
+pkg add /root/iconik_storage_gateway-latest.txz
 
 # Enable iconik ui
 echo "iconik_ui_enable=\"YES\"" >> /etc/rc.conf
