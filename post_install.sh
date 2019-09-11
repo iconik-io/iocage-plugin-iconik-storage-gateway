514ea4ef4965cdea567db5e6fdc45b37f8bb8ba9
Waqar Ahmed <waqarahmedjoyia@live.com>
2019-09-11 20:17:49 +0500

    Update post install script

--- post_install.sh
+++ post_install.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
+# Enable iconik storage gateway
 sysrc -f /etc/rc.conf iconik_storage_gateway_enable="YES"
-
-echo "Download Distfiles"
-cd /root
-fetch https://downloads.iconik.io/iconik_storage_gateway-latest.txz
-
-pkg add /root/iconik_storage_gateway-latest.txz
 
 # Enable iconik ui
 echo "iconik_ui_enable=\"YES\"" >> /etc/rc.conf
@@ -14,4 +9,3 @@ echo "iconik_ui_enable=\"YES\"" >> /etc/rc.conf
 #restart services
 /usr/local/etc/rc.d/iconik_ui restart
 /usr/local/etc/rc.d/iconik_storage_gateway restart
-
